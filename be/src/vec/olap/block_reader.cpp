apache/doris

[Bug](pipeline) fix pipeline shared scan + topn optimization

be/src/vec/olap/block_reader.cpp
@@ -60,8 +60,9 @@ BlockReader::~BlockReader() {
     }
 }
 
-bool BlockReader::_rowsets_overlapping(const std::vector<RowsetReaderSharedPtr>& rs_readers) {
+bool BlockReader::_rowsets_overlapping(const ReaderParams& read_params) {
     std::string cur_max_key;
+    const std::vector<RowsetReaderSharedPtr>& rs_readers = read_params.rs_readers;
     for (const auto& rs_reader : rs_readers) {
         // version 0-1 of every tablet is empty, just skip this rowset
         if (rs_reader->rowset()->version().second == 1) {
@@ -83,6 +84,30 @@ bool BlockReader::_rowsets_overlapping(const std::vector<RowsetReaderSharedPtr>&
         }
         CHECK(rs_reader->rowset()->max_key(&cur_max_key));
     }
+
+    const std::vector<RowSetReaderWithSegments>& rs_readers_with_segments =
+            read_params.rs_readers_with_segments;
+    for (const auto& rs_reader : rs_readers_with_segments) {
+        // version 0-1 of every tablet is empty, just skip this rowset
+        if (rs_reader.rs_reader->rowset()->version().second == 1) {
+            continue;
+        }
+        if (rs_reader.rs_reader->rowset()->num_rows() == 0) {
+            continue;
+        }
+        if (rs_reader.rs_reader->rowset()->is_segments_overlapping()) {
+            return true;
+        }
+        std::string min_key;
+        bool has_min_key = rs_reader.rs_reader->rowset()->min_key(&min_key);
+        if (!has_min_key) {
+            return true;
+        }
+        if (min_key <= cur_max_key) {
+            return true;
+        }
+        CHECK(rs_reader.rs_reader->rowset()->max_key(&cur_max_key));
+    }
     return false;
 }
 Status BlockReader::_init_collect_iter(const ReaderParams& read_params) {
@@ -96,25 +121,19 @@ Status BlockReader::_init_collect_iter(const ReaderParams& read_params) {
         return res;
     }
     // check if rowsets are noneoverlapping
-    _is_rowsets_overlapping = _rowsets_overlapping(read_params.rs_readers);
+    _is_rowsets_overlapping = _rowsets_overlapping(read_params);
     _vcollect_iter.init(this, _is_rowsets_overlapping, read_params.read_orderby_key,
-                        read_params.read_orderby_key_reverse,
-                        read_params.rs_readers_segment_offsets);
+                        read_params.read_orderby_key_reverse, read_params.scanner_idx);
 
     _reader_context.push_down_agg_type_opt = read_params.push_down_agg_type_opt;
     std::vector<RowsetReaderSharedPtr> valid_rs_readers;
-    DCHECK(read_params.rs_readers_segment_offsets.empty() ||
-           read_params.rs_readers_segment_offsets.size() == read_params.rs_readers.size());
 
-    bool is_empty = read_params.rs_readers_segment_offsets.empty();
     for (int i = 0; i < read_params.rs_readers.size(); ++i) {
         auto& rs_reader = read_params.rs_readers[i];
 
         // _vcollect_iter.topn_next() will init rs_reader by itself
         if (!_vcollect_iter.use_topn_next()) {
-            RETURN_IF_ERROR(rs_reader->init(
-                    &_reader_context,
-                    is_empty ? std::pair {0, 0} : read_params.rs_readers_segment_offsets[i]));
+            RETURN_IF_ERROR(rs_reader->init(&_reader_context));
         }
 
         Status res = _vcollect_iter.add_child(rs_reader);
@@ -124,6 +143,24 @@ Status BlockReader::_init_collect_iter(const ReaderParams& read_params) {
         }
         if (res.ok()) {
             valid_rs_readers.push_back(rs_reader);
+        }
+    }
+
+    for (int i = 0; i < read_params.rs_readers_with_segments.size(); ++i) {
+        auto& rs_reader = read_params.rs_readers_with_segments[i];
+
+        // _vcollect_iter.topn_next() will init rs_reader by itself
+        if (!_vcollect_iter.use_topn_next()) {
+            RETURN_IF_ERROR(rs_reader.rs_reader->init(&_reader_context, 0, rs_reader));
+        }
+
+        Status res = _vcollect_iter.add_child(rs_reader);
+        if (!res.ok() && !res.is<END_OF_FILE>()) {
+            LOG(WARNING) << "failed to add child to iterator, err=" << res;
+            return res;
+        }
+        if (res.ok()) {
+            valid_rs_readers.push_back(rs_reader.rs_reader);
         }
     }
 

be/src/vec/olap/vcollect_iterator.h
@@ -54,9 +54,11 @@ public:
     ~VCollectIterator();
 
     void init(TabletReader* reader, bool ori_data_overlapping, bool force_merge, bool is_reverse,
-              std::vector<std::pair<int, int>> rs_readers_segment_offsets);
+              size_t scanner_idx);
 
     Status add_child(RowsetReaderSharedPtr rs_reader);
+
+    Status add_child(const RowSetReaderWithSegments& rs_readers_with_segments);
 
     Status build_heap(std::vector<RowsetReaderSharedPtr>& rs_readers);
     // Get top row of the heap, nullptr if reach end.
@@ -332,10 +334,11 @@ private:
     bool _topn_eof = false;
     // when we use scanner pooling + query with topn_with_limit, we use it.
     std::vector<RowsetReaderSharedPtr> _rs_readers;
-    std::vector<std::pair<int, int>> _rs_readers_segment_offsets;
+    std::vector<RowSetReaderWithSegments> _rs_readers_with_segments;
 
     // Hold reader point to access read params, such as fetch conditions.
     TabletReader* _reader = nullptr;
+    size_t _scanner_idx;
 
     bool _skip_same;
 };

Gabriel39/incubator-doris

be/src/vec/olap/block_reader.cpp
@@ -60,8 +60,9 @@ BlockReader::~BlockReader() {
     }
 }
 
-bool BlockReader::_rowsets_overlapping(const std::vector<RowsetReaderSharedPtr>& rs_readers) {
+bool BlockReader::_rowsets_overlapping(const ReaderParams& read_params) {
     std::string cur_max_key;
+    const std::vector<RowsetReaderSharedPtr>& rs_readers = read_params.rs_readers;
     for (const auto& rs_reader : rs_readers) {
         // version 0-1 of every tablet is empty, just skip this rowset
         if (rs_reader->rowset()->version().second == 1) {
@@ -83,6 +84,29 @@ bool BlockReader::_rowsets_overlapping(const std::vector<RowsetReaderSharedPtr>&
         }
         CHECK(rs_reader->rowset()->max_key(&cur_max_key));
     }
+
+    const std::vector<RowSetSplits>& rs_splits = read_params.rs_splits;
+    for (const auto& rs_reader : rs_splits) {
+        // version 0-1 of every tablet is empty, just skip this rowset
+        if (rs_reader.rs_reader->rowset()->version().second == 1) {
+            continue;
+        }
+        if (rs_reader.rs_reader->rowset()->num_rows() == 0) {
+            continue;
+        }
+        if (rs_reader.rs_reader->rowset()->is_segments_overlapping()) {
+            return true;
+        }
+        std::string min_key;
+        bool has_min_key = rs_reader.rs_reader->rowset()->min_key(&min_key);
+        if (!has_min_key) {
+            return true;
+        }
+        if (min_key <= cur_max_key) {
+            return true;
+        }
+        CHECK(rs_reader.rs_reader->rowset()->max_key(&cur_max_key));
+    }
     return false;
 }
 Status BlockReader::_init_collect_iter(const ReaderParams& read_params) {
@@ -96,25 +120,19 @@ Status BlockReader::_init_collect_iter(const ReaderParams& read_params) {
         return res;
     }
     // check if rowsets are noneoverlapping
-    _is_rowsets_overlapping = _rowsets_overlapping(read_params.rs_readers);
+    _is_rowsets_overlapping = _rowsets_overlapping(read_params);
     _vcollect_iter.init(this, _is_rowsets_overlapping, read_params.read_orderby_key,
-                        read_params.read_orderby_key_reverse,
-                        read_params.rs_readers_segment_offsets);
+                        read_params.read_orderby_key_reverse, read_params.scanner_idx);
 
     _reader_context.push_down_agg_type_opt = read_params.push_down_agg_type_opt;
     std::vector<RowsetReaderSharedPtr> valid_rs_readers;
-    DCHECK(read_params.rs_readers_segment_offsets.empty() ||
-           read_params.rs_readers_segment_offsets.size() == read_params.rs_readers.size());
 
-    bool is_empty = read_params.rs_readers_segment_offsets.empty();
     for (int i = 0; i < read_params.rs_readers.size(); ++i) {
         auto& rs_reader = read_params.rs_readers[i];
 
         // _vcollect_iter.topn_next() will init rs_reader by itself
         if (!_vcollect_iter.use_topn_next()) {
-            RETURN_IF_ERROR(rs_reader->init(
-                    &_reader_context,
-                    is_empty ? std::pair {0, 0} : read_params.rs_readers_segment_offsets[i]));
+            RETURN_IF_ERROR(rs_reader->init(&_reader_context));
         }
 
         Status res = _vcollect_iter.add_child(rs_reader);
@@ -124,6 +142,24 @@ Status BlockReader::_init_collect_iter(const ReaderParams& read_params) {
         }
         if (res.ok()) {
             valid_rs_readers.push_back(rs_reader);
+        }
+    }
+
+    for (int i = 0; i < read_params.rs_splits.size(); ++i) {
+        auto& rs_reader = read_params.rs_splits[i];
+
+        // _vcollect_iter.topn_next() will init rs_reader by itself
+        if (!_vcollect_iter.use_topn_next()) {
+            RETURN_IF_ERROR(rs_reader.rs_reader->init(&_reader_context, 0, rs_reader));
+        }
+
+        Status res = _vcollect_iter.add_child(rs_reader);
+        if (!res.ok() && !res.is<END_OF_FILE>()) {
+            LOG(WARNING) << "failed to add child to iterator, err=" << res;
+            return res;
+        }
+        if (res.ok()) {
+            valid_rs_readers.push_back(rs_reader.rs_reader);
         }
     }
 

be/src/vec/olap/vcollect_iterator.h
@@ -54,9 +54,11 @@ public:
     ~VCollectIterator();
 
     void init(TabletReader* reader, bool ori_data_overlapping, bool force_merge, bool is_reverse,
-              std::vector<std::pair<int, int>> rs_readers_segment_offsets);
+              size_t scanner_idx);
 
     Status add_child(RowsetReaderSharedPtr rs_reader);
+
+    Status add_child(const RowSetSplits& rs_splits);
 
     Status build_heap(std::vector<RowsetReaderSharedPtr>& rs_readers);
     // Get top row of the heap, nullptr if reach end.
@@ -332,10 +334,11 @@ private:
     bool _topn_eof = false;
     // when we use scanner pooling + query with topn_with_limit, we use it.
     std::vector<RowsetReaderSharedPtr> _rs_readers;
-    std::vector<std::pair<int, int>> _rs_readers_segment_offsets;
+    std::vector<RowSetSplits> _rs_splits;
 
     // Hold reader point to access read params, such as fetch conditions.
     TabletReader* _reader = nullptr;
+    size_t _scanner_idx;
 
     bool _skip_same;
 };