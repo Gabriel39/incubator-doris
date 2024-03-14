66c980595ff463b8c3afdb34ffe2a2fddf73a498
Gabriel <gabrielleebuaa@gmail.com>
2024-03-14 15:11:06 +0800

    udpate

--- be/src/vec/aggregate_functions/aggregate_function_uniq.h
+++ be/src/vec/aggregate_functions/aggregate_function_uniq.h
@@ -210,7 +210,8 @@ public:
             set.insert(ref);
         }
         int64_t start3 = MonotonicNanos();
-        LOG(WARNING) << "=======1 " << (start3 - start2) << " " << (start2 - start);
+        LOG(WARNING) << "=======1 " << (start3 - start2) << " " << (start2 - start1) << " "
+                     << (start1 - start);
     }
 
     void deserialize(AggregateDataPtr __restrict place, BufferReadable& buf,
